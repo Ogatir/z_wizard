b3e613e18042a46b81c2c5b82ce46908a3eb8515
karasev <karasev@crypta>
2018-12-03 22:51:16 +0300

    Примитивный интерфейс

--- gui/src/main/java/Main.java
+++ gui/src/main/java/Main.java
@@ -1,6 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
 
     public static void main(String args[]){
         System.out.println("Z-wizard begins his magic");
+
+        MainWindow main_window = new MainWindow();
+        main_window.pack();
+        main_window.setSize(new Dimension(500,500));
+        main_window.setVisible(true);
+        main_window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
